776766d9cd05bfb532252b815263498f43386b88
Jordan <jordan.l.partridge@gmail.com>
2024-06-03 22:27:12 -0700

    add an option to the filter drawer

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use App\Models\Country;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Livewire\Volt\Component;
@@ -11,6 +12,8 @@ new class extends Component {
     use WithPagination;
     use Toast;
 
+
+    public int $country_id = 0;
 
     public string $search = '';
 
@@ -27,9 +30,10 @@ new class extends Component {
     }
 
     // Delete action
-    public function delete($id): void
+    public function delete(User $user): void
     {
-        $this->warning("Will delete #$id", 'It is fake.', position: 'toast-bottom');
+        $user->delete();
+        $this->warning("$user->name deleted", 'Good bye!', position: 'toast-bottom');
     }
 
     // Table headers
@@ -38,7 +42,7 @@ new class extends Component {
         return [
             ['key' => 'id', 'label' => '#', 'class' => 'w-1'],
             ['key' => 'name', 'label' => 'Name', 'class' => 'w-64'],
-            ['key' => 'country_name', 'label' => 'Country'],
+            ['key' => 'country_name', 'label' => 'Country', 'class' => 'hidden lg:table-cell'],
             ['key' => 'email', 'label' => 'E-mail', 'sortable' => false],
         ];
     }
@@ -56,6 +60,7 @@ new class extends Component {
             ->withAggregate('country', 'name')
             ->with(['country'])
             ->when($this->search, fn(Builder $q) => $q->where('name', 'like', "%$this->search%"))
+            ->when($this->country_id, fn(Builder $q) => $q->where('country_id', $this->country_id))
             ->orderBy(...array_values($this->sortBy))
             ->paginate(5);
     }
@@ -63,7 +68,7 @@ new class extends Component {
     // Reset pagination when any component property changes
     public function updated($property): void
     {
-        if (! is_array($property) && $property != "") {
+        if (!is_array($property) && $property != "") {
             $this->resetPage();
         }
     }
@@ -71,8 +76,9 @@ new class extends Component {
     public function with(): array
     {
         return [
-            'users'   => $this->users(),
-            'headers' => $this->headers()
+            'users'     => $this->users(),
+            'headers'   => $this->headers(),
+            'countries' => Country::all(),
         ];
     }
 }; ?>
@@ -101,7 +107,9 @@ new class extends Component {
     <!-- FILTER DRAWER -->
     <x-drawer wire:model="drawer" title="Filters" right separator with-close-button class="lg:w-1/3">
         <x-input placeholder="Search..." wire:model.live.debounce="search" icon="o-magnifying-glass"
-                 @keydown.enter="$wire.drawer = false"/>
+                 @keydown.enter="$wire.drawer = false"/>        <x-select placeholder="Country" wire:model.live="country_id" :options="$countries" icon="o-flag" placeholder-value="0" />
+        <x-select placeholder="Country" wire:model.live="country_id" :options="$countries" icon="o-flag" placeholder-value="0" />
+
 
         <x-slot:actions>
             <x-button label="Reset" icon="o-x-mark" wire:click="clear" spinner/>
